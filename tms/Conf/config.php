6cbd894b9b16f77cad2d7c0dfff7d417fc947366
Boxin Wang <me@co8bit.com>
2014-05-20 02:41:24 +0800

    edit readme.md

--- tms/Conf/config.php
+++ tms/Conf/config.php
@@ -1,8 +1,17 @@
 <?php
-//注意，请不要在这里配置SAE的数据库，配置你本地的数据库就可以了。
 return array(
-    //'配置项'=>'配置值'
-    'SHOW_PAGE_TRACE'=>true,
-    'URL_HTML_SUFFIX'=>'.html'
+	//'配置项'=>'配置值'
+	'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
+	
+	// 数据库配置信息
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_NAME'   => 'sts', // 数据库名
+	'DB_USER'   => 'sts', // 用户名
+	'DB_PWD'    => 'sts', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => 'a6_', // 数据库表前缀
+	
+	'SESSION_AUTO_START'    => true,    // 是否自动开启Session
 );
 ?>
---
+++ tms/Lib/Action/BaseAction.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class IndexAction extends Action {
+
+    protected function _initialize() {
+        header("Content-Type:text/html; charset=utf-8");
+    }
+
+    public function index()
+    {
+        $this->display();
+    }
+}
+
+?>
