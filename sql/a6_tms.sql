f3fe40cb6f5231514d3e90d42724f8c4bbf065b9
co8bit <co8bit@gmail.com>
2014-05-22 22:25:19 +0800

    a6:修改数据表

--- sql/a6_tms.sql
+++ sql/a6_tms.sql
@@ -34,7 +34,15 @@ create table a6_stock_info(
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 insert a6_stock_info values(null,"新浪科技",1,12,2,0.05,0.05);
 insert a6_stock_info values(null,"国家电网",1,23,65,0.05,0.05);
-
+insert a6_stock_info values(null,"新浪",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"百度",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"搜狐",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"亚马逊",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"宝钢",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"中石油",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"中国电信",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"中国移动",1,23,65,0.05,0.05);
+insert a6_stock_info values(null,"中国联通",1,23,65,0.05,0.05);
 
 create table a6_user_auth(
 	uid bigint NOT NULL,
@@ -45,8 +53,26 @@ create table a6_user_auth(
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 insert a6_user_auth values(1,1);
 insert a6_user_auth values(1,2);
+insert a6_user_auth values(1,3);
+insert a6_user_auth values(1,4);
+insert a6_user_auth values(1,5);
+insert a6_user_auth values(1,6);
+insert a6_user_auth values(1,7);
+insert a6_user_auth values(1,8);
+insert a6_user_auth values(1,9);
+insert a6_user_auth values(1,10);
+insert a6_user_auth values(1,11);
 insert a6_user_auth values(2,1);
-
+insert a6_user_auth values(2,2);
+insert a6_user_auth values(2,3);
+insert a6_user_auth values(2,4);
+insert a6_user_auth values(2,5);
+insert a6_user_auth values(2,6);
+insert a6_user_auth values(2,7);
+insert a6_user_auth values(2,8);
+insert a6_user_auth values(2,9);
+insert a6_user_auth values(2,10);
+insert a6_user_auth values(2,11);
 
 /*
  * 测试用
@@ -61,5 +87,15 @@ create table a4_instruct(
 	primary key(id)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 insert a4_instruct values(null,1,1,12.3,45,"2014-05-29 20:30:23");
-insert a4_instruct values(null,1,0,3,90,"2014-05-29 20:32:23");
+insert a4_instruct values(null,1,0,3,90,"2014-05-29 22:32:23");
+insert a4_instruct values(null,1,1,15.3,415,"2014-05-29 20:30:23");
+insert a4_instruct values(null,1,0,36,930,"2014-05-29 12:32:23");
+insert a4_instruct values(null,1,1,123.3,445,"2014-05-11 20:30:23");
+insert a4_instruct values(null,1,0,32,940,"2014-05-29 09:32:23");
+insert a4_instruct values(null,1,1,112.3,435,"2014-05-08 20:30:23");
+insert a4_instruct values(null,1,0,33,902,"2014-05-29 07:32:23");
+insert a4_instruct values(null,1,1,122.3,435,"2014-05-06 20:30:23");
+insert a4_instruct values(null,1,0,33,960,"2014-05-29 13:32:23");
+insert a4_instruct values(null,1,1,142.3,445,"2014-05-14 20:30:23");
+insert a4_instruct values(null,1,0,35,940,"2014-05-29 15:32:23");
 insert a4_instruct values(null,2,1,12.5,23,"2014-05-29 20:40:23");
